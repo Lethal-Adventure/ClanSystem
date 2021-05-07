0242e8458b33ea10b671a384e59993a3d420787a
Overflow <77083630+KaneSeries2@users.noreply.github.com>
2021-05-07 09:23:45 -0400

    sys config

---
+++ Plugin/src/main/java/sys/system/System.java
@@ -0,0 +1,17 @@
+package sys.system;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class System extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
